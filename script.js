57b75da0e103474f4b8a71a07859db6814a059e2
Greg Roberts <gregorysroberts@gmail.com>
2014-02-04 19:46:42 -0700

    Adds append method to array

--- script.js
+++ script.js
@@ -7,6 +7,22 @@ $(document).ready(function() {
 			}
 		}
 	).done(function(response){
-		console.log(results[0].title);
+		//Create a for loop that will loop through the array and append each title and body
+		console.log(response.results.length);
+		for (var i = 0; i < response.results.length; i++) {
+			console.log(response.results[i].title);
+			console.log(response.results[i].body);
+
+			//Append a div with classname newsitem that contains an h1 with the title and a p containing the body
+			var newsFeed = 
+			"<div class=newsitem>" +
+				"<h1>" + response.results[i].title + "</h1>" +
+				"<p>" + response.results[i].body + "</p>" +
+			"</div>"
+
+
+
+			$('.container').append(newsFeed);
+		}
 	});
 });
