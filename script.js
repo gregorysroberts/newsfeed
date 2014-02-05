7f1304f7f609d42b473e96df30cb1265008d4a32
Greg Roberts <gregorysroberts@gmail.com>
2014-02-04 19:22:40 -0700

    Adds .ajax call

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-
+	$.ajax('https://api.parse.com/1/classes/NewsItem', 
+		{
+			beforeSend: function (request) {
+				request.setRequestHeader('X-Parse-Application-Id', 'p1BfzzXQ5fvRb6i60kmuUUerC3ObIZOR4tnMXQLN');
+				request.setRequestHeader('X-Parse-REST-API-Key', 'aKicpnI01E66EXLGhsbADQUBVqnoP66SDGehJV4y');
+			}
+		}
+	).done(function(response){
+		console.log(results[0].title);
+	});
 });
